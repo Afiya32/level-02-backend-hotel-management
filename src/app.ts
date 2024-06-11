d517eb0574dc725dec81214db73f4de33ad95e80
Afiya32 <biplobguru123@gmail.com>
2024-06-12 00:14:42 +0600

    basic  setup done now hotel management server is running

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,14 @@
-import express, { Application, Request, Response } from "express"
-import cors from "cors"
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
 
-
-
-
-const app : Application = express()
-const port = 3000
-
+const app: Application = express();
+const port = 3000;
 
 // parser
-app.use(express.json())
-app.use(cors())
-app.get('/', (req:Request, res:Response) => {
-  res.send('Hello World!')
-})
+app.use(express.json());
+app.use(cors());
+app.get("/", (req: Request, res: Response) => {
+  res.send("welcome to hotel management server ");
+});
 
 export default app;
