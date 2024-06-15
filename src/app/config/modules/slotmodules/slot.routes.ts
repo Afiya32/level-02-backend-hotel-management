8aad3cd1eb33d46c5eef9d94297ca5a04b688557
Afiya32 <biplobguru123@gmail.com>
2024-06-15 14:14:35 +0600

    final overlook done and post some data

--- src/app/config/modules/slotmodules/slot.routes.ts
+++ src/app/config/modules/slotmodules/slot.routes.ts
@@ -2,12 +2,13 @@
 
 import express from 'express';
 import { slotController } from './slot.controller';
+import { authenticateAdmin } from '../../auth.middleware';
 
 
 const SlotRouter = express.Router();
 
 // Route for creating a slot
-SlotRouter.post('/slots', slotController.createSlot);
+SlotRouter.post('/slots',authenticateAdmin, slotController.createSlot);
 
 // Route for getting available slots
 SlotRouter.get('/slots/availability', slotController.getAvailableSlots);
